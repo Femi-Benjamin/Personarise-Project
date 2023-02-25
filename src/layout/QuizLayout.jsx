15c0552d207bfc2ae2b413e795693ae64d3a1edf
kelani34 <kelanitaiwo34@gmail.com>
2023-02-25 13:13:37 +0100

    update: add all quiz questions

--- src/layout/QuizLayout.jsx
+++ src/layout/QuizLayout.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../components/shared/Button";
 import { quizQuestions } from "../constants/data";
+import { addScore, removeScore } from "../redux/users/userAction";
 
 function Quizlayout() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
   const [timerSeconds, setTimerSeconds] = useState(720); // 12 minutes = 720 seconds
   const [timerIntervalId, setTimerIntervalId] = useState(null);
+  const score = useSelector((state) => state.user.score);
+  let keepScore = 0;
 
+  const dispatch = useDispatch();
   useEffect(() => {
     if (timerSeconds === 0) {
       clearInterval(timerIntervalId);
@@ -46,10 +51,11 @@ function Quizlayout() {
   return (
     <>
       <div className="bg bg-no-repeat bg-slate-100 h-full md:flex justify-center items-center ">
+        <h1>{score}</h1>
         <div className=" md:h-auto h-full  bg-[#FFFFFF] md:m-3">
           <div className="md:px-[92px] md:py-[47px] p-6 flex flex-col justify-center items-center not-italic font-mulish m-3">
             {/* PROGRESS BAR */}
-            <div class="w-[60%] bg-[#E4FEE3] rounded-full h-2.5  ">
+            <div class="w-[80%] bg-[#E4FEE3] rounded-full h-5  ">
               <div
                 className="bg-[#51A54B] h-5 rounded-full"
                 style={{
@@ -65,31 +71,37 @@ function Quizlayout() {
               {Math.floor(timerSeconds / 60)}:{timerSeconds % 60}
             </h1>
             {/* QUESTION */}
+            <h2 className=" text-2xl max-w-[400px] text-center mb-3">
+              {quizQuestions[currentQuestionIndex].title}
+            </h2>
             <p className="pb-[32.71px] font-bold md:text-xl text-sm leading-[26px] text-black">
               {quizQuestions[currentQuestionIndex].question}
             </p>
             <div className="grid md:grid-cols-2 sm:grid-cols-1 justify-between gap-[20.51px] md:mb-[50px] mb-5">
               {quizQuestions[currentQuestionIndex].options.map(
-                (option, index) => (
-                  <label
-                    key={index}
-                    htmlFor={`option ${index}`}
-                    className={`border-[0.850816px] border-[#CDD2D5] hover:border-[#51A54B] md:w-[263.75px] w-[200px] md:h-[83.38px] h-[50px] font-bold md:text-[16px] text-xs leading-[20px] flex justify-center items-center cursor-pointer selection:${
-                      selectedOptionIndex === index
-                        ? " bg-[#071D2E] text-white"
-                        : ""
-                    }`}
-                    onClick={() => handleOptionSelect(index)}
-                  >
-                    {option}
-                    <input
-                      type="radio"
-                      id={`option ${index}`}
-                      checked={selectedOptionIndex === index}
-                      className="hidden"
-                    />
-                  </label>
-                )
+                (option, index) => {
+                  keepScore = option.score;
+                  return (
+                    <label
+                      key={index}
+                      htmlFor={`option ${option.id}`}
+                      className={`border-[0.850816px] border-[#CDD2D5] hover:border-[#51A54B] md:w-[263.75px] w-[200px] md:h-[83.38px] h-[50px] font-bold md:text-[16px] text-xs leading-[20px] flex justify-center items-center p-2 text-center cursor-pointer selection:${
+                        selectedOptionIndex === option.id
+                          ? " bg-[#071D2E] text-white"
+                          : ""
+                      }`}
+                      onClick={() => handleOptionSelect(option.id)}
+                    >
+                      {option.text}
+                      <input
+                        type="radio"
+                        id={`option ${option.id}`}
+                        checked={selectedOptionIndex === option.id}
+                        className="hidden"
+                      />
+                    </label>
+                  );
+                }
               )}
             </div>
             <div
@@ -101,7 +113,10 @@ function Quizlayout() {
                 <div className="">
                   <Button
                     text="Previous Question"
-                    onClick={handlePreviousQuestion}
+                    onClick={() => {
+                      handlePreviousQuestion();
+                      dispatch(removeScore(keepScore));
+                    }}
                     outline={true}
                     disabled={currentQuestionIndex === 0}
                   />
@@ -110,9 +125,11 @@ function Quizlayout() {
               )}
               <div className="w-full">
                 <Button
-                  onClick={
-                    isLastQuestion ? handleFormSubmit : handleNextQuestion
-                  }
+                  onClick={() => {
+                    isLastQuestion ? handleFormSubmit() : handleNextQuestion();
+                    dispatch(addScore(keepScore));
+                  }}
+                  disabled={!selectedOptionIndex}
                   text={isLastQuestion ? "Submit" : "Next Question"}
                 />
               </div>
